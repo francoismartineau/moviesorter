2c126155529f33231b95be6d4628836e1c5d589a
Francois Martineau <ffrancoismmartineau@gmail.com>
2022-08-02 12:47:34 -0400

    fix static root url attempt

--- moviesorter_project/urls.py
+++ moviesorter_project/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
-from django.contrib.staticfiles.views import serve
+#from django.conf.urls import url
+#from django.contrib.staticfiles.views import serve
 from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('moviesorter.urls')),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
